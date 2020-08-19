ef9988f58ff6981959936abb4255047773e6bdac
Pietro da Silva Klug <diheguiparker@gmail.com>
2020-08-19 11:23:56 -0300

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,47 @@
+//Variables return a value: String, Number or boolean(False, True)
+var name = "Pietro";
+var age = 20;
+var haveSchool = false;
+
+//if the variable don't exists, return an error "Variable not defined".
+console.log(name, age, haveSchool);
+
+var price = 25;
+var products = 5;
+var totalPrice = products * price;
+
+console.log(totalPrice);
+
+//It is not necessary to repeat "var" to create more than one variable, just comma.
+var lastName = "Klug",
+  city = "SLS";
+
+console.log(lastName, city);
+
+var something;
+//Variable without value return undefined.
+console.log(something);
+
+//Variables declared after the console return undefined
+console.log(food);
+
+//Variables declared before the console return the value of variable.
+var food = "Pizza";
+console.log(comida);
+
+//redefinition of variable
+var team = "Gremio";
+team = "Flamengo";
+console.log(team);
+
+//Exercises
+
+// Declare an variable with your name
+
+// Declare an variable with your age
+
+//Declare an variable with your favorite food without value
+
+//Assign a value
+
+//Declare five diferent variables without value
